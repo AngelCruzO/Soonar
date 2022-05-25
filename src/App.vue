0c6cbf032fb67a789a0ce2be10163e9a7f0a4d8e
AngelCruzO <vce08201@innovaccion.mx>
2022-05-24 19:57:20 -0500

    add: file Menu component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Menu/>
 </template>
+
+<script>
+import Menu from '@/components/Menu.vue'
+
+export default {
+  name: 'App',
+  components:{
+    Menu
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <div class="logo">
+            <h1>Soonar</h1>
+        </div>
+        <nav>
+            <li>Inicio</li>
+            <li>Ecosistema</li>
+            <li>Voluntariado</li>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Menu'
+}
+</script>
