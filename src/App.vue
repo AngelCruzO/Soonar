f734ba08c3f3beda99fa44e0c8b4359f585c0ce0
AngelCruzO <vce08201@innovaccion.mx>
2022-05-25 21:17:42 -0500

    update: Menu, Abang font

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
 
 <script>
 import Menu from '@/components/Menu.vue'
-
 export default {
   name: 'App',
   components:{
@@ -13,25 +12,4 @@ export default {
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,12 +1,12 @@
 <template>
     <header>
         <div class="logo">
-            <h1>Soonar</h1>
+            <h1><a href="">Soonar</a></h1>
         </div>
         <nav>
-            <li>Inicio</li>
-            <li>Ecosistema</li>
-            <li>Voluntariado</li>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Ecosistema</a></li>
+            <li><a href="">Voluntariado</a></li>
         </nav>
     </header>
 </template>
@@ -16,3 +16,64 @@ export default {
     name: 'Menu'
 }
 </script>
+
+<style>
+
+/* GENERALES */
+
+@font-face {
+    font-family: 'Abang Regular';
+    font-style: normal;
+    font-weight: normal;
+    src: local('Abang Regular'), url('../assets/fonts/Abang.woff') format('woff');
+}
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+header{
+    height: 90px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: #3333;
+}
+
+nav{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 500px;
+}
+
+li{
+    list-style: none;
+}
+
+a{
+    text-decoration: none;
+}
+
+/* ESPECIFICOS */
+
+.logo{
+    font-family: 'Abang Regular';
+    font-size: 25px;
+    margin-left: 2em;
+    color: #7BC789;
+}
+
+h1 a{
+    color: #7BC789;
+}
+
+li a{
+    color: #fff;
+    font-size: 24px;
+}
+
+
+</style>
